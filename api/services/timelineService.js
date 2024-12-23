3c2cd7f976d0225823298fad5eb7538b1fbde95e
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-12-23 01:35:18 +0100

    feat: add 'POST,PUT, DELETE' request handlers for timeline endpoints

--- api/services/timelineService.js
+++ api/services/timelineService.js
@@ -14,6 +14,39 @@ class TimelineService {
       return { error: error.message };
     }
   }
+
+  static async createTimelineEvent(data) {
+    try {
+      const newTimelineEvent = await TimelineEvents.create(data);
+      return newTimelineEvent;
+    } catch (error) {
+      throw new Error("Error creating timeline event: " + error.message);
+    }
+  }
+  static async updateTimelineEvent(id, updatedData) {
+    try {
+      const timelineEvent = await TimelineEvents.findByPk(id);
+      if (!timelineEvent) {
+        return { error: "Timeline event not found" };
+      }
+      const updatedTimelineEvent = await timelineEvent.update(updatedData);
+      return updatedTimelineEvent;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+  static async deleteTimelineEvent(id) {
+    try {
+      const timelineEvent = await TimelineEvents.findByPk(id);
+      if (!timelineEvent) {
+        return { error: "Timeline event not found" };
+      }
+      await timelineEvent.destroy();
+      return { message: "Timeline event deleted successfully" };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
 }
 
 export default TimelineService;
--- api/src/routers/timelineRouter.js
+++ api/src/routers/timelineRouter.js
@@ -3,6 +3,9 @@ import TimelineController from "../../controllers/timelineController.js";
 
 const timelineRouter = express.Router();
 
-timelineRouter.get("/:id/timeline", TimelineController.getTimelineEventsByProject);
+timelineRouter.get("/project/:id/", TimelineController.getTimelineEventsByProject);
+timelineRouter.post("/", TimelineController.createTimelineEvent);
+timelineRouter.put("/:id", TimelineController.updateTimelineEvent);
+timelineRouter.delete("/:id", TimelineController.deleteTimelineEvent);
 
 export default timelineRouter;
